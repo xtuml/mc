c2ff962d9be0a3ff0374db7d2d784548de3f6554
Cortland Starrett <cort@roxsoftware.com>
2013-07-31 16:16:32 -0400

    job:  #4  Updated auxiliary files for the RSL parser to convert to OAL.  Also a list is supplied of the archetypes that do convert now.

---
+++ mcmc/arlan/current_list.sh
@@ -0,0 +1,16 @@
+rsl2oal < ../../arc/q.parm.sort.arc > o.oal
+rsl2oal < ../../arc/q.rel.pseudoformalize.arc >> o.oal
+rsl2oal < ../../arc/c/q.sys.singletons.arc >> o.oal
+rsl2oal < ../../arc/q.datatype.arc >> o.oal
+rsl2oal < ../../arc/q.parameters.arc >> o.oal
+rsl2oal < ../../arc/q.sys.populate.arc >> o.oal
+rsl2oal < ../../arc/q.utils.arc >> o.oal
+rsl2oal < ../../arc/m.datatype.arc >> o.oal
+rsl2oal < ../../arc/m.system.arc >> o.oal
+rsl2oal < ../../arc/q.domain.analyze.arc >> o.oal
+rsl2oal < ../../arc/q.oal.analyze.arc >> o.oal
+rsl2oal < ../../arc/q.oal.utils.arc >> o.oal
+rsl2oal < ../../arc/q.domain.limits.arc >> o.oal
+
+# Second list for afunval.
+rsl2oal < ../../arc/q.val.translate.arc > o2.oal
