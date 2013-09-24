36332bafb452f58ef482277292b6cdcf9cfd3fdb
Cortland Starrett <cort@roxsoftware.com>
2013-09-24 17:02:42 -0400

    job:  #4  Added the translated templates.  Also adding ooafooa.c, because it gets a few hand edits.

---
+++ model/com.mentor.nucleus.bp.core/gen/c/t.smt_sr.init_set.c
@@ -0,0 +1,5 @@
+T_b(ws);
+T_b(te_set->init);
+T_b("( ");
+T_b(te_select_related->result_var);
+T_b(" );\n");
---
+++ model/com.mentor.nucleus.bp.core/gen/c/t.smt_sr.result_set_init.c
@@ -0,0 +1,6 @@
+T_b(ws);
+T_b(te_set->module);
+T_b(te_set->clear);
+T_b("( ");
+T_b(te_select_related->result_var);
+T_b(" );\n");
