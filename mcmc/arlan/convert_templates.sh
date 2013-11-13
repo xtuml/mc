d1048f805feff43c09b0bccf2c94c7e5b4d91241
keithbrown <keith_brown@mentor.com>
2013-11-13 09:17:25 -0500

    job #4 - use gawk-style double-slashing.

--- mcmc/arlan/convert_templates.sh
+++ mcmc/arlan/convert_templates.sh
@@ -6,7 +6,7 @@ filelist=`ls ../../arc/t.*.[ch] | xargs -n1 basename`
 for f in $filelist; do
   cat  ../../arc/$f | sed "s/^.assign[^\"]*\"\(.*\)\"/\1/" | \
   ./template_engine | \
-  awk '{if ($0 ~ "if ") {gsub(" and "," \&\& ");gsub(" or ", " || ");gsub(" not "," ! ");gsub("\.","->");} print $0;}' | \
+  awk '{if ($0 ~ "if ") {gsub(" and "," \\&\\& ");gsub(" or ", " || ");gsub(" not "," ! ");gsub("\\.","->");} print $0;}' | \
   sed 's/\(T_b[^"]*\)"\(.*\)"\([^"]*\)/\1\&quot;\2\&quot;\3/' | \
   awk '{if ($0 ~ "T_b") gsub("\"","\\\"");print $0;}' | \
   sed 's/&quot;/"/g' | \
