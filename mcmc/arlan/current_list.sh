423684375837224969afc96dcc648c7f6190a020
keithbrown <keith_brown@mentor.com>
2014-04-30 14:40:06 -0400

    job #59 - Fixed redirects

--- mcmc/arlan/current_list.sh
+++ mcmc/arlan/current_list.sh
@@ -1,12 +1,12 @@
 # first for afun
-./rsl2oal < ../../arc/frag_util.arc | sed 's/include(file:"${te_file.arc_path}/include(file:"c/' | sh swapreflex.sh >> o.oal
+./rsl2oal < ../../arc/frag_util.arc | sed 's/include(file:"${te_file.arc_path}/include(file:"c/' | sh swapreflex.sh > o.oal
 ./rsl2oal < ../../arc/q.datatype.arc | sh swapreflex.sh >> o.oal
 ./rsl2oal < ../../arc/q.domain.analyze.arc | sh swapreflex.sh >> o.oal
 ./rsl2oal < ../../arc/q.domain.limits.arc | sh swapreflex.sh >> o.oal
 ./rsl2oal < ../../arc/q.oal.analyze.arc | sh swapreflex.sh >> o.oal
 ./rsl2oal < ../../arc/q.oal.utils.arc | sh swapreflex.sh >> o.oal
 ./rsl2oal < ../../arc/q.parameters.arc | sh swapreflex.sh >> o.oal
-./rsl2oal < ../../arc/q.parm.sort.arc | sh swapreflex.sh > o.oal
+./rsl2oal < ../../arc/q.parm.sort.arc | sh swapreflex.sh >> o.oal
 ./rsl2oal < ../../arc/q.rel.pseudoformalize.arc | sh swapreflex.sh >> o.oal
 ./rsl2oal < ../../arc/q.sys.populate.arc | sh swapreflex.sh >> o.oal
 ./rsl2oal < ../../arc/q.utils.arc | sh swapreflex.sh >> o.oal
@@ -16,10 +16,10 @@
 ./rsl2oal < ../../arc/q.val.translate.arc | sed 's/""""/"\&quot;"/g' | sh swapreflex.sh > o2.oal
 
 # third list for afunsmt
-./rsl2oal < ../../arc/q.names.arc | sed 's/include(file:"${te_file.arc_path}/include(file:"c/' | sh swapreflex.sh >> o3.oal
+./rsl2oal < ../../arc/q.names.arc | sed 's/include(file:"${te_file.arc_path}/include(file:"c/' | sh swapreflex.sh > o3.oal
 ./rsl2oal < ../../arc/q.oal.act_blk.arc | sed 's/, """/, \&quot;"/' | sed 's/+ """/+ "\&quot;/' | sed 's/include(file:"${te_file.arc_path}/include(file:"c/' | sh swapreflex.sh >> o3.oal
 ./rsl2oal < ../../arc/q.oal.action.arc | sed 's/include(file:"${te_file.arc_path}/include(file:"c/' | sh swapreflex.sh >> o3.oal
-./rsl2oal < ../../arc/q.oal.translate.arc | sed 's/include(file:"${te_file.arc_path}/include(file:"c/' | sh swapreflex.sh > o3.oal
+./rsl2oal < ../../arc/q.oal.translate.arc | sed 's/include(file:"${te_file.arc_path}/include(file:"c/' | sh swapreflex.sh >> o3.oal
 ./rsl2oal < ../../arc/q.smt.generate.arc | sed 's/include(file:"${te_file.arc_path}/include(file:"c/' | sh swapreflex.sh >> o3.oal
 ./rsl2oal < ../../arc/q.r_rel.arc | sed 's/include(file:"${te_file.arc_path}/include(file:"c/' | sh swapreflex.sh >> o3.oal
 
