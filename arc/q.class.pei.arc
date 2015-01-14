xtuml/mc

commit 210eea234ef5f33ed19e1d3f4138ac2c91b320ea

Added the instance dumping routines to each class. This change also makes the sequencing/ordering of parameters more efficient.  This is done with a reflexive on TE_ATTR.  #4

arc/q.class.pei.arc
@@ -124,30 +124,27 @@
   .//
   .// Generate the attribute value initializers.
   .//
-  .invoke first_attr = GetFirstAttributeInObjectModel( o_obj )
-  .assign current_attr = first_attr.result
-  .if ( not_empty current_attr )
-    .select one te_attr related by current_attr->TE_ATTR[R2033]
-    .while ( not_empty current_attr )
+  .select any te_attr related by o_obj->TE_CLASS[R2019]-TE_ATTR[R2061] where ( selected.prevID == 0 )
+  .if ( not_empty te_attr )
+    .select one o_attr related by te_attr->O_ATTR[R2033]
+    .while ( not_empty te_attr )
       .if ( te_attr.translate )
-        .select any i_avl related by i_ins->I_AVL[R2909] where ( selected.Attr_ID == current_attr.Attr_ID )
-        .invoke avlcode = PEIRenderInitializerBodyAttribute( current_attr, i_avl )
+        .select any i_avl related by i_ins->I_AVL[R2909] where ( selected.Attr_ID == o_attr.Attr_ID )
+        .invoke avlcode = PEIRenderInitializerBodyAttribute( o_attr, i_avl )
         .assign attr_result = attr_result + avlcode.result
       .end if
       .//
       .// Advance to the next object attribute, if any.
-      .select one next_attr related by current_attr->O_ATTR[R103.'succeeds']
-      .assign current_attr = next_attr
+      .select one te_attr related by te_attr->TE_ATTR[R2087.'succeeds']
       .//
       .// Generate comma separator.
-      .if ( not_empty current_attr )
-        .select one te_attr related by current_attr->TE_ATTR[R2033]
+      .if ( not_empty te_attr )
         .if ( te_attr.translate )
           .assign attr_result = attr_result + ", "
         .end if
       .end if
     .end while
-  .end if .// not_empty current_attr
+  .end if
   .//
   .// Generate the association linkage (if needed).
   .//

arc/frag_util.arc
@@ -77,25 +77,24 @@ ${indent.result}}
 .// and therefore need to be automatically set up at create time.
 .//============================================================================
 .function AutoInitializeUniqueIDs
-  .param inst_ref o_obj
+  .param inst_ref te_class
   .param string instance
   .//
   .assign attr_result = FALSE
   .select any te_instance from instances of TE_INSTANCE
   .select any te_string from instances of TE_STRING
-  .invoke first_attr = GetFirstAttributeInObjectModel( o_obj )
-  .assign current_attr = first_attr.result
-  .while ( not_empty current_attr )
-    .select one te_attr related by current_attr->TE_ATTR[R2033]
+  .select any te_attr related by te_class->TE_ATTR[R2061] where ( selected.prevID == 0 )
+  .while ( not_empty te_attr )
+    .select one o_attr related by te_attr->O_ATTR[R2033]
     .if ( te_attr.translate )
-      .invoke member_type = GetAttributeCodeGenType( current_attr )
+      .invoke member_type = GetAttributeCodeGenType( o_attr )
       .assign cdt = member_type.cdt
       .if ( not_empty cdt )
       .if ( cdt.Core_Typ == 5 )
         .assign dt = member_type.dt
         .// Core_Typ == 5 is "unique_id"
         .// CDS:  Note "select any" when there may be more than one.
-        .select any oida related by current_attr->O_OIDA[R105]
+        .select any oida related by o_attr->O_OIDA[R105]
         .if ( not_empty oida )
           .select one te_dt related by dt->TE_DT[R2021]
           .assign attr_result = TRUE
@@ -116,9 +115,8 @@ ${te_instance.module}${te_string.strcpy}( ${instance}->${te_attr.GeneratedName},
     .end if
     .//
     .// Advance to the next object attribute, if any.
-    .select one next_attr related by current_attr->O_ATTR[R103.'succeeds']
-    .assign current_attr = next_attr
-  .end while  .// not_empty current_attr
+    .select one te_attr related by te_attr->TE_ATTR[R2087.'succeeds']
+  .end while
 .end function
 .//
 .//============================================================================
@@ -162,10 +160,8 @@ ${compare_stmt}\
   .assign param_list = ""
   .assign ident_attr_count = 0
   .//
-  .invoke first_attr = GetFirstAttributeInObjectModel( o_obj )
-  .assign current_attr = first_attr.result
-  .while ( not_empty current_attr )
-    .select one te_attr related by current_attr->TE_ATTR[R2033]
+  .select any te_attr related by o_obj->TE_CLASS[R2019]->TE_ATTR[R2061] where ( selected.prevID == 0 )
+  .while ( not_empty te_attr )
     .if ( te_attr.Included )
       .assign ident_attr_count = ident_attr_count + 1
       .assign param_list = param_list + te_attr.ParamBuffer
@@ -173,8 +169,7 @@ ${compare_stmt}\
         .assign param_list = param_list + ", "
       .end if
     .end if
-    .select one next_attr related by current_attr->O_ATTR[R103.'succeeds']
-    .assign current_attr = next_attr
+    .select one te_attr related by te_attr->TE_ATTR[R2087.'succeeds']
   .end while
   .//
 ${param_list}\

arc/q.class.arc
@@ -20,13 +20,12 @@
 .// Creates the prologue for the implementation files
 .//============================================================================
 .function CreateFilePrologue
-  .param inst_ref o_obj
+  .param inst_ref te_class
   .param boolean  gen_declaration
   .//
   .select any te_copyright from instances of TE_COPYRIGHT
   .select any te_file from instances of TE_FILE
   .select any te_target from instances of TE_TARGET
-  .select one te_class related by o_obj->TE_CLASS[R2019]
   .select one te_c related by te_class->TE_C[R2064]
 /*----------------------------------------------------------------------------
   .if ( gen_declaration )
@@ -73,17 +72,16 @@ ${te_target.c2cplusplus_linkage_end}
 .// appear in the application analysis class diagram.
 .//============================================================================
 .function CreateObjectAttrDataDeclaration
-  .param inst_ref o_obj
+  .param inst_ref te_class
   .select any te_string from instances of TE_STRING
-  .invoke first_attr = GetFirstAttributeInObjectModel( o_obj )
-  .assign current_attr = first_attr.result
-  .while ( not_empty current_attr )
-    .select one te_attr related by current_attr->TE_ATTR[R2033]
-    .select one te_dt related by current_attr->S_DT[R114]->TE_DT[R2021]
+  .select any te_attr related by te_class->TE_ATTR[R2061] where ( selected.prevID == 0 )
+  .while ( not_empty te_attr )
+    .select one o_attr related by te_attr->O_ATTR[R2033]
+    .select one te_dt related by o_attr->S_DT[R114]->TE_DT[R2021]
     .assign comment = ""
     .select any te_sys from instances of TE_SYS
     .if ( not te_sys.InstanceLoading )
-    .invoke note = GetObjectAttributeInfoComment( current_attr, TRUE )
+    .invoke note = GetObjectAttributeInfoComment( o_attr, TRUE )
     .if ( note.result != "" )
       .assign comment = "/* ${note.result} */"
     .end if
@@ -93,7 +91,7 @@ ${te_target.c2cplusplus_linkage_end}
       .if ( te_attr.translate )
         .if ( te_dt.Core_Typ == 2 )
           .// integer type
-          .assign bit_field_width = "${current_attr.Descrip:BIT_WIDTH}"
+          .assign bit_field_width = "${o_attr.Descrip:BIT_WIDTH}"
           .if ( "${bit_field_width}" != "" )
   ui_t ${te_attr.GeneratedName} : ${bit_field_width};  ${comment}
           .else
@@ -107,8 +105,7 @@ ${te_target.c2cplusplus_linkage_end}
       .end if
     .end if
     .// Advance to the next object attribute, if any.
-    .select one next_attr related by current_attr->O_ATTR[R103.'succeeds']
-    .assign current_attr = next_attr
+    .select one te_attr related by te_attr->TE_ATTR[R2087.'succeeds']
   .end while
 .end function
 .//
@@ -206,7 +203,7 @@ ${te_target.c2cplusplus_linkage_end}
       .assign cs = persist.current_state
     .end if
     .// *** Application OIM data members
-    .invoke data_members = CreateObjectAttrDataDeclaration( o_obj )
+    .invoke data_members = CreateObjectAttrDataDeclaration( te_class )
     .assign abody = "  /* application analysis class attributes */\n"
     .assign abody = abody + data_members.body
     .//
@@ -327,7 +324,7 @@ class ${te_c.Name}; // forward reference
   .select one o_obj related by te_class->O_OBJ[R2019]
   .select one sm_ism related by o_obj->SM_ISM[R518]
   .select one sm_asm related by o_obj->SM_ASM[R519]
-  .invoke file_prologue = CreateFilePrologue( o_obj, gen_declaration )
+  .invoke file_prologue = CreateFilePrologue( te_class, gen_declaration )
   .invoke file_epilogue = CreateFileEpilogue( te_class, gen_declaration )
   .assign instance_loader = ""
   .assign batch_relation = ""
@@ -353,6 +350,7 @@ ${file_prologue.body}\
 ${include_files.body}\
   .if ( gen_declaration )
 ${obj_data_class.body}\
+    .include "${te_file.arc_path}/t.class.instancedumper.h"
   .end if
 ${instance_loader}\
 ${batch_relation}\
@@ -364,6 +362,7 @@ ${rel_frag.body}
 };
     .end if
   .else
+    .include "${te_file.arc_path}/t.class.instancedumper.c"
     .if ( "" != xforms.body )
       .include "${te_file.arc_path}/t.class.ops.c"
     .end if

arc/q.class.factory.arc
@@ -74,7 +74,7 @@
   .select any te_instance from instances of TE_INSTANCE
   .select one te_class related by o_obj->TE_CLASS[R2019]
   .if ( gen_declaration )
-extern ${te_instance.handle} ${te_class.GeneratedName}_instanceloader( ${te_instance.handle}, const c_t * [] );
+${te_instance.handle} ${te_class.GeneratedName}_instanceloader( ${te_instance.handle}, const c_t * [] );
   .else
 
 /*
@@ -86,14 +86,13 @@ ${te_class.GeneratedName}_instanceloader( ${te_instance.handle} instance, const 
   ${te_instance.handle} return_identifier = 0;
   ${te_class.GeneratedName} * ${te_instance.self} = (${te_class.GeneratedName} *) instance;
   /* Initialize application analysis class attributes.  */
-    .invoke first_attr = GetFirstAttributeInObjectModel( o_obj )
-    .assign current_attr = first_attr.result
+    .select any te_attr related by te_class->TE_ATTR[R2061] where ( selected.prevID == 0 )
     .assign attribute_number = 1
-    .while ( not_empty current_attr )
-      .select one te_attr related by current_attr->TE_ATTR[R2033]
-      .assign persistent = ( "$l{current_attr.Descrip:Persistent}" != "false" )
+    .while ( not_empty te_attr )
+      .select one o_attr related by te_attr->O_ATTR[R2033]
+      .assign persistent = ( "$l{o_attr.Descrip:Persistent}" != "false" )
       .if ( te_attr.translate and persistent )
-        .invoke member_type = GetAttributeCodeGenType( current_attr )
+        .invoke member_type = GetAttributeCodeGenType( o_attr )
         .assign cdt = member_type.cdt
         .assign dt = member_type.dt
         .if ( empty cdt )
@@ -124,8 +123,8 @@ ${te_class.GeneratedName}_instanceloader( ${te_instance.handle} instance, const 
         .elif ( cdt.Core_Typ == 5 )
           .// unique_id
   ${te_instance.self}->${te_attr.GeneratedName} = (${te_instance.handle}) ${te_instance.module}${te_string.atoi}( avlstring[ ${attribute_number} ] );
-          .select any o_oida related by current_attr->O_OIDA[R105] where ( selected.Oid_ID == 0 )
-          .select one o_rattr related by current_attr->O_RATTR[R106]
+          .select any o_oida related by o_attr->O_OIDA[R105] where ( selected.Oid_ID == 0 )
+          .select one o_rattr related by o_attr->O_RATTR[R106]
           .if ( ( not_empty o_oida ) and ( empty o_rattr ) )
   return_identifier = ${te_instance.self}->${te_attr.GeneratedName};
           .end if
@@ -157,12 +156,11 @@ ${te_class.GeneratedName}_instanceloader( ${te_instance.handle} instance, const 
         .else
   ${te_instance.module}${te_string.memset}( &${te_instance.self}->${te_attr.GeneratedName}, avlstring[ ${attribute_number} ], sizeof( ${te_instance.self}->${te_attr.GeneratedName} ) );
         .end if
-      .end if  .// code_needed.result
+      .end if
       .//
       .// Advance to the next object attribute, if any.
-      .select one next_attr related by current_attr->O_ATTR[R103.'succeeds']
-      .assign current_attr = next_attr
-    .end while  .// not_empty current_attr
+      .select one te_attr related by te_attr->TE_ATTR[R2087.'succeeds']
+    .end while
   return return_identifier;
 }
   .end if  .// gen_declaration
@@ -179,7 +177,7 @@ ${te_class.GeneratedName}_instanceloader( ${te_instance.handle} instance, const 
   .if ( not_empty r_rgos )
     .invoke extent_info = GetFixedSizeClassExtentInfo( o_obj )
     .if ( gen_declaration )
-extern void ${te_class.GeneratedName}_batch_relate( ${te_instance.handle} );
+void ${te_class.GeneratedName}_batch_relate( ${te_instance.handle} );
     .else
 
 /*
@@ -235,24 +233,23 @@ void ${te_class.GeneratedName}_batch_relate( ${te_instance.handle} instance )
           .assign param_list = ""
           .assign delimeter = ""
           .assign nonreferential = false
-          .invoke first_attr = GetFirstAttributeInObjectModel( rto_obj )
-          .assign current_attr = first_attr.result
-          .while ( not_empty current_attr )
+          .select any te_attr related by part_te_class->TE_ATTR[R2061] where ( selected.prevID == 0 )
+          .while ( not_empty te_attr )
+            .select one current_o_attr related by te_attr->O_ATTR[R2033]
             .for each o_rtida in o_rtidas
               .select one o_attr related by o_rtida->O_OIDA[R110]->O_ATTR[R105]
-              .if ( o_attr == current_attr )
-                .select one o_rattr related by current_attr->O_RATTR[R106]
+              .if ( o_attr == current_o_attr )
+                .select one o_rattr related by current_o_attr->O_RATTR[R106]
                 .if ( empty o_rattr )
                   .assign nonreferential = true
                 .end if
                 .select any o_attr related by o_rtida->O_REF[R111]->O_RATTR[R108]->O_ATTR[R106] where ( selected.Obj_ID == r_rgo.Obj_ID )
-                .select one te_attr related by o_attr->TE_ATTR[R2033]
-                .assign param_list = param_list + "${delimeter}${te_class.GeneratedName}_instance->${te_attr.GeneratedName}"
+                .select one ref_te_attr related by o_attr->TE_ATTR[R2033]
+                .assign param_list = param_list + "${delimeter}${te_class.GeneratedName}_instance->${ref_te_attr.GeneratedName}"
                 .assign delimeter = ", "
               .end if
             .end for
-            .select one next_attr related by current_attr->O_ATTR[R103.'succeeds']
-            .assign current_attr = next_attr
+            .select one te_attr related by te_attr->TE_ATTR[R2087.'succeeds']
           .end while
           .if ( ( ( 0 == o_id.Oid_ID ) and ( 1 == o_rtida_count ) ) and nonreferential )
   ${part_te_class.GeneratedName} * ${part_te_class.GeneratedName}related_instance${r_rto_count} = (${part_te_class.GeneratedName} *) Escher_instance_cache[ (int) ${param_list} ];

arc/q.class.link.arc
@@ -1412,26 +1412,6 @@ ${aoth_fundamentals.body}\
 .end function
 .//
 .//============================================================================
-.// Return <result> an instance reference of type O_ATTR to the first
-.// (e.g., top) attribute of an object in the anlysis OIM.
-.//============================================================================
-.function GetFirstAttributeInObjectModel
-  .param inst_ref o_obj
-  .//
-  .// Get a no such instance attribute for variable type declaration.
-  .select any attr_result related by o_obj->O_ATTR[R102] where ( selected.PAttr_ID == 0 )
-  .// Find the first attribute of the object (e.g., no R103 predecessor)
-  .select many obj_attr_set related by o_obj->O_ATTR[R102]
-  .for each obj_attr in obj_attr_set
-    .select one previous_attr related by obj_attr->O_ATTR[R103.'precedes']
-    .if ( empty previous_attr )
-      .assign attr_result = obj_attr
-      .break for
-    .end if
-  .end for
-.end function
-.//
-.//============================================================================
 .// Return <result> the multiplicity/conditionality string.
 .//
 .// Inputs:

arc/q.class.sem.arc
@@ -395,7 +395,7 @@
     .invoke poly_rels = GetPolymorphicRelationshipCollection( o_obj )
     .assign poly_rel_set = poly_rels.result
     .//
-    .invoke init_uniques = AutoInitializeUniqueIDs( o_obj, "i" )
+    .invoke init_uniques = AutoInitializeUniqueIDs( te_class, "i" )
     .invoke max_event = GetMaxEventInfo( o_obj, TRUE )
     .invoke domain_id = GetDomainTypeIDFromString( te_c.Name )
     .invoke poly_dispatch = CreatePolymorphicEventDispatcher( o_obj, poly_rel_set )

arc/q.class.where.arc
@@ -36,25 +36,22 @@
   .assign cmp_element = ""
   .assign temp_ptr = "w"
   .//
-  .select many te_attrs related by o_obj->O_ATTR[R102]->TE_ATTR[R2033]
+  .select many te_attrs related by te_class->TE_ATTR[R2061]
   .for each te_attr in te_attrs
     .assign te_attr.Included = FALSE
   .end for
   .//
   .// Tag the indentifying attribute(s) that comprise this where clause.
-  .select many ident_attr_set related by o_oid->O_OIDA[R105]->O_ATTR[R105]
-  .assign num_ident_attr = cardinality ident_attr_set
-  .for each ident_attr in ident_attr_set
-    .select one te_attr related by ident_attr->TE_ATTR[R2033]
+  .select many ident_te_attrs related by o_oid->O_OIDA[R105]->O_ATTR[R105]->TE_ATTR[R2033]
+  .assign num_ident_attr = cardinality ident_te_attrs
+  .for each te_attr in ident_te_attrs
     .assign te_attr.Included = TRUE
-    .assign te_attr.ParamBuffer = "w_${ident_attr.Name}"
+    .assign te_attr.ParamBuffer = "w_${te_attr.Name}"
   .end for
   .//
-  .invoke first_attr = GetFirstAttributeInObjectModel( o_obj )
-  .assign current_attr = first_attr.result
+  .select any te_attr related by te_class->TE_ATTR[R2061] where ( selected.prevID == 0 )
   .assign ident_attr_count = 0
-  .while ( not_empty current_attr )
-    .select one te_attr related by current_attr->TE_ATTR[R2033]
+  .while ( not_empty te_attr )
     .if ( te_attr.Included )
       .assign ident_attr_count = ident_attr_count + 1
       .assign param_list = param_list + te_attr.GeneratedType
@@ -76,15 +73,15 @@
         .assign compare_stmt = compare_stmt + " && "
       .end if
     .end if
-    .select one next_attr related by current_attr->O_ATTR[R103.'succeeds']
-    .assign current_attr = next_attr
+    .select one te_attr related by te_attr->TE_ATTR[R2087.'succeeds']
   .end while
   .//
   .if ( gen_declaration )
     .include "${te_file.arc_path}/t.class.select_where.h"
   .else
+    .select one o_obj related by te_class->O_OBJ[R2019]
     .invoke extent_info = GetFixedSizeClassExtentInfo( o_obj )
-    .assign iterator = "iter_${o_obj.Key_Lett}"
+    .assign iterator = "iter_${te_class.Key_Lett}"
     .include "${te_file.arc_path}/t.class.select_where.c"
   .end if  .// gen_declaration
 .end function

arc/q.mc_metamodel.populate.arc
@@ -42,6 +42,7 @@
   .select any empty_te_c from instances of TE_C where ( false )
   .select any empty_te_dim from instances of TE_DIM where ( false )
   .select any empty_o_obj from instances of O_OBJ where ( false )
+  .select any empty_te_attr from instances of TE_ATTR where ( false )
   .select any empty_te_mact from instances of TE_MACT where ( false )
   .select many empty_te_dts from instances of TE_DT where ( false )
   .select many empty_te_parms from instances of TE_PARM where ( false )
@@ -1427,8 +1428,10 @@
       .assign te_class.persist_link = te_class.GeneratedName + "_LinkCentral"
       .//
       .// Create the Generated Attribute instances and link them to the real ones.
-      .select many o_attrs related by o_obj->O_ATTR[R102]
-      .for each o_attr in o_attrs
+      .assign delimiter = ""
+      .assign prev_te_attr = empty_te_attr
+      .select any o_attr related by o_obj->O_ATTR[R102] where ( selected.PAttr_ID == 0 )
+      .while ( not_empty o_attr )
         .create object instance te_attr of TE_ATTR
         .assign te_attr.Name = o_attr.Name
         .assign te_attr.GeneratedName = "$r{o_attr.Name}"
@@ -1445,6 +1448,11 @@
         .assign te_attr.Attr_ID = o_attr.Attr_ID
         .// relate te_attr to te_class across R2061;
         .assign te_attr.te_classGeneratedName = te_class.GeneratedName
+        .assign te_attr.prevID = 0
+        .if ( not_empty prev_te_attr )
+          .// relate te_attr to prev_te_attr across R2087.'succeeds';
+          .assign te_attr.prevID = prev_te_attr.ID
+        .end if
         .select many s_dims related by o_attr->S_DIM[R120]
         .assign array_spec = ""
         .assign te_attr.dimensions = cardinality s_dims
@@ -1464,6 +1472,8 @@
         .assign te_attr.array_spec = array_spec
         .select one te_dt related by o_attr->S_DT[R114]->TE_DT[R2021]
         .assign te_attr.GeneratedType = te_dt.ExtName
+        .assign te_class.attribute_format = ( te_class.attribute_format + delimiter ) + te_dt.string_format
+        .assign te_class.attribute_dump = ( te_class.attribute_dump + ",\n    self->" ) + te_attr.GeneratedName
         .if ( 7 == te_dt.Core_Typ )
           .// referential attribute
           .invoke a = GetAttributeCodeGenType( o_attr )
@@ -1496,7 +1506,10 @@
           .// relate te_dbattr to te_aba across R2010;
           .assign te_dbattr.AbaID = te_aba.AbaID
         .end if
-      .end for
+        .assign delimiter = ","
+        .assign prev_te_attr = te_attr
+        .select one o_attr related by o_attr->O_ATTR[R103.'succeeds']
+      .end while
       .//
       .// Create the Generated State Machines and connect them to SM_SM.
       .select one sm_sm related by o_obj->SM_ISM[R518]->SM_SM[R517]

commit 86fbf8aabe2962a9b7641849853c82ce5c75cdb0

fixed a syntax error introduced by 210eea23

arc/q.class.pei.arc
@@ -124,7 +124,7 @@
   .//
   .// Generate the attribute value initializers.
   .//
-  .select any te_attr related by o_obj->TE_CLASS[R2019]-TE_ATTR[R2061] where ( selected.prevID == 0 )
+  .select any te_attr related by o_obj->TE_CLASS[R2019]->TE_ATTR[R2061] where ( selected.prevID == 0 )
   .if ( not_empty te_attr )
     .select one o_attr related by te_attr->O_ATTR[R2033]
     .while ( not_empty te_attr )