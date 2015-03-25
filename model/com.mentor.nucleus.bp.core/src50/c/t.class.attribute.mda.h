efeace58c43c0e9bb3d9af4d50ed9a253430f2be
Cortland Starrett <cort@roxsoftware.com>
2015-03-25 18:35:01 -0400

    job:  #714  Adding source for the 5.0 release of mcmc.  A first draft analysis note for structured messaging is introduced.

---
+++ model/com.mentor.nucleus.bp.core/src50/c/t.class.attribute.mda.h
@@ -0,0 +1,7 @@
+T_b(te_aba->ReturnDataType);
+T_b(" ");
+T_b(te_aba->GeneratedName);
+T_b("( ");
+T_b(te_class->GeneratedName);
+T_b(" * );");
+T_b("\n");
---
+++ model/com.mentor.nucleus.bp.core/src50/c/t.smt.control.c
@@ -0,0 +1,5 @@
+if ( 0!=strcmp("C++",te_target->language) ) {
+T_b(ws);
+T_b("ARCH_shutdown();");
+T_b("\n");
+}
