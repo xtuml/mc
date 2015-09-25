513651dc9f92911314ac6a57c4edeb7f42ebbefc
Cortland Starrett <cort@roxsoftware.com>
2015-09-24 21:47:23 -0400

    job:  #7880  Fixed typo in function call.

--- arc/c/t.component.message.c
+++ arc/c/t.component.message.c
@@ -24,7 +24,7 @@ ${te_aba.scope}${te_aba.GeneratedName}(${te_aba.ParameterDefinition})
     .end if
   .end if
   .if ( te_mact.trace )
-    .invoke r = GetDomainTypeIDFromSTring( te_c.Name )
+    .invoke r = GetDomainTypeIDFromString( te_c.Name )
     .assign dom_id = r.result
   /* <message compname="${te_c.Name}" compnum="${te_c.number}" portname="${te_po.Name}" portnum="${te_po.Order}" msgname="${te_mact.MessageName}" msgnum="${te_mact.Order}"/> */
   ${te_trace.component_msg_start}( "${te_aba.ParameterFormat}", ${dom_id}, ${te_po.Order}, ${te_mact.Order}${te_aba.ParameterTrace} );
