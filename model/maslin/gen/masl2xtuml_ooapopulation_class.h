975c1360c0d068c72a0956e91d89dfc12ed485be
Keith Brown <s.keith.brown@gmail.com>
2016-02-23 16:09:14 -0500

    job #8241 - Added a lot of infrastructure for handling class associations.  Initial support for simple associations is present.

--- model/maslin/gen/masl2xtuml_ooapopulation_class.h
+++ model/maslin/gen/masl2xtuml_ooapopulation_class.h
@@ -32,6 +32,7 @@ struct masl2xtuml_ooapopulation {
   masl2xtuml_S_SYNC * current_domain_function;  
   masl2xtuml_S_SPARM * current_function_param;  
   masl2xtuml_O_TPARM * current_operation_param;  
+  masl2xtuml_sdt_relationship_data current_rel;  
   i_t processingIdentifier;  
   bool processingISM;  
 };
@@ -109,6 +110,10 @@ void masl2xtuml_ooapopulation_op_Function_addParameterToOrder( masl2xtuml_ooapop
 void masl2xtuml_ooapopulation_op_Function_createMessageArgumentsForParameter( masl2xtuml_ooapopulation *, masl2xtuml_S_SPARM *, masl2xtuml_S_SYNC * );
 void masl2xtuml_ooapopulation_op_FunctionParameter_setType( masl2xtuml_ooapopulation *, masl2xtuml_S_SPARM *, c_t * );
 void masl2xtuml_ooapopulation_op_OperationParameter_setType( masl2xtuml_ooapopulation *, masl2xtuml_O_TPARM *, c_t * );
+void masl2xtuml_ooapopulation_op_clearRelationshipCache( masl2xtuml_ooapopulation * );
+void masl2xtuml_ooapopulation_op_Package_newAssociation( masl2xtuml_ooapopulation *, masl2xtuml_EP_PKG * );
+void masl2xtuml_ooapopulation_op_Association_initialize( masl2xtuml_ooapopulation *, masl2xtuml_R_REL * );
+void masl2xtuml_ooapopulation_op_transformAssociation( masl2xtuml_ooapopulation * );
 
 
 #define masl2xtuml_ooapopulation_MAX_EXTENT_SIZE 10
