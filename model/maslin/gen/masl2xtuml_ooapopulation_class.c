c4476db0c0594fbcdfe2db4389388211f0343281
Levi Starrett <levi@roxsoftware.com>
2016-03-22 11:29:01 -0400

    job #8371 added null checks to protect from segfaults related to the element stack

--- model/maslin/gen/masl2xtuml_ooapopulation_class.c
+++ model/maslin/gen/masl2xtuml_ooapopulation_class.c
@@ -78,17 +78,26 @@ masl2xtuml_ooapopulation_op_populate( c_t * p_element, c_t p_value[8][ESCHER_SYS
     masl2xtuml_ooaelement * current_element=0;
     /* SELECT one current_element RELATED BY ooapopulation->ooaelement[R3801] */
     current_element = ( 0 != ooapopulation ) ? ooapopulation->ooaelement_R3801_has_current : 0;
-    /* IF ( ooaelement::ismarkable(type:element) ) */
-    if ( masl2xtuml_ooaelement_op_ismarkable(element) ) {
-      masl2xtuml_ooamarkable * markable=0;
-      /* SELECT one markable RELATED BY current_element->ooamarkable[R3806] */
-      markable = 0;
-      if ( ( 0 != current_element ) && ( masl2xtuml_ooamarkable_CLASS_NUMBER == current_element->R3806_object_id ) )      markable = ( 0 != current_element ) ? (masl2xtuml_ooamarkable *) current_element->R3806_subtype : 0;
-      /* markable.render_pragmas() */
-      masl2xtuml_ooamarkable_op_render_pragmas( markable );
+    /* IF ( not_empty current_element ) */
+    if ( ( 0 != current_element ) ) {
+      /* IF ( ooaelement::ismarkable(type:element) ) */
+      if ( masl2xtuml_ooaelement_op_ismarkable(element) ) {
+        masl2xtuml_ooamarkable * markable=0;
+        /* SELECT one markable RELATED BY current_element->ooamarkable[R3806] */
+        markable = 0;
+        if ( ( 0 != current_element ) && ( masl2xtuml_ooamarkable_CLASS_NUMBER == current_element->R3806_object_id ) )        markable = ( 0 != current_element ) ? (masl2xtuml_ooamarkable *) current_element->R3806_subtype : 0;
+        /* markable.render_pragmas() */
+        masl2xtuml_ooamarkable_op_render_pragmas( markable );
+      }
+      /* current_element.pop_stack() */
+      masl2xtuml_ooaelement_op_pop_stack( current_element );
     }
-    /* current_element.pop_stack() */
-    masl2xtuml_ooaelement_op_pop_stack( current_element );
+    else {
+      /* TRACE::log( flavor:failure, id:55, message:Element stack is malformed. ) */
+      TRACE_log( "failure", 55, "Element stack is malformed." );
+      /* ooaelement::trace( population:ooapopulation ) */
+      masl2xtuml_ooaelement_op_trace( ooapopulation );
+    }
   }
   else if ( !( ( ( ( ( ( Escher_strcmp( "typeref", element ) == 0 ) || ( Escher_strcmp( "referential", element ) == 0 ) ) || ( Escher_strcmp( "pragmaitem", element ) == 0 ) ) || ( Escher_strcmp( "transition", element ) == 0 ) ) || ( Escher_strcmp( "expression", element ) == 0 ) ) || ( Escher_strcmp( "codeblock", element ) == 0 ) ) ) {
     /* ooaelement::populate( population:ooapopulation, type:element ) */
@@ -520,14 +529,6 @@ masl2xtuml_ooapopulation_op_populate( c_t * p_element, c_t p_value[8][ESCHER_SYS
     }
   }
   else if ( ( Escher_strcmp( "pragma", element ) == 0 ) ) {
-    masl2xtuml_ooaelement * parent_element=0;
-    /* SELECT one parent_element RELATED BY ooapopulation->ooaelement[R3801]->ooaelement[R3805.child of] */
-    parent_element = 0;
-    {    if ( 0 != ooapopulation ) {
-    masl2xtuml_ooaelement * ooaelement_R3801_has_current = ooapopulation->ooaelement_R3801_has_current;
-    if ( 0 != ooaelement_R3801_has_current ) {
-    parent_element = ooaelement_R3801_has_current->ooaelement_R3805_child_of;
-}}}
     /* IF ( (  == PARAM.value[0] ) ) */
     if ( ( Escher_strcmp( "", p_value[0] ) == 0 ) ) {
       masl2xtuml_ooapragma * pragma=0;
@@ -536,23 +537,52 @@ masl2xtuml_ooapopulation_op_populate( c_t * p_element, c_t p_value[8][ESCHER_SYS
       /* ASSIGN ooapopulation.current_pragma = pragma */
       ooapopulation->current_pragma = pragma;
     }
-    else if ( masl2xtuml_ooaelement_op_ismarkable(parent_element->type) ) {
-      masl2xtuml_ooamarkable * markable=0;
-      /* SELECT one markable RELATED BY parent_element->ooamarkable[R3806] */
-      markable = 0;
-      if ( ( 0 != parent_element ) && ( masl2xtuml_ooamarkable_CLASS_NUMBER == parent_element->R3806_object_id ) )      markable = ( 0 != parent_element ) ? (masl2xtuml_ooamarkable *) parent_element->R3806_subtype : 0;
-      /* ASSIGN ooapopulation.current_pragma = ooapragma::populate(list:value[1], markable:markable, name:value[0]) */
-      ooapopulation->current_pragma = masl2xtuml_ooapragma_op_populate(value[1], markable, value[0]);
+    else {
+      masl2xtuml_ooaelement * parent_element=0;
+      /* SELECT one parent_element RELATED BY ooapopulation->ooaelement[R3801]->ooaelement[R3805.child of] */
+      parent_element = 0;
+      {      if ( 0 != ooapopulation ) {
+      masl2xtuml_ooaelement * ooaelement_R3801_has_current = ooapopulation->ooaelement_R3801_has_current;
+      if ( 0 != ooaelement_R3801_has_current ) {
+      parent_element = ooaelement_R3801_has_current->ooaelement_R3805_child_of;
+}}}
+      /* IF ( not_empty parent_element ) */
+      if ( ( 0 != parent_element ) ) {
+        /* IF ( ooaelement::ismarkable(type:parent_element.type) ) */
+        if ( masl2xtuml_ooaelement_op_ismarkable(parent_element->type) ) {
+          masl2xtuml_ooamarkable * markable=0;
+          /* SELECT one markable RELATED BY parent_element->ooamarkable[R3806] */
+          markable = 0;
+          if ( ( 0 != parent_element ) && ( masl2xtuml_ooamarkable_CLASS_NUMBER == parent_element->R3806_object_id ) )          markable = ( 0 != parent_element ) ? (masl2xtuml_ooamarkable *) parent_element->R3806_subtype : 0;
+          /* ASSIGN ooapopulation.current_pragma = ooapragma::populate(list:value[1], markable:markable, name:value[0]) */
+          ooapopulation->current_pragma = masl2xtuml_ooapragma_op_populate(value[1], markable, value[0]);
+        }
+      }
+      else {
+        /* TRACE::log( flavor:failure, id:56, message:Element stack is malformed. ) */
+        TRACE_log( "failure", 56, "Element stack is malformed." );
+        /* ooaelement::trace( population:ooapopulation ) */
+        masl2xtuml_ooaelement_op_trace( ooapopulation );
+      }
     }
   }
   else if ( ( Escher_strcmp( "pragmaitem", element ) == 0 ) ) {
     masl2xtuml_ooaelement * current_element=0;
     /* SELECT one current_element RELATED BY ooapopulation->ooaelement[R3801] */
     current_element = ( 0 != ooapopulation ) ? ooapopulation->ooaelement_R3801_has_current : 0;
-    /* IF ( ( pragma == current_element.type ) ) */
-    if ( ( Escher_strcmp( "pragma", current_element->type ) == 0 ) ) {
-      /* ooapragma_item::populate( pragma:ooapopulation.current_pragma, value:value[0] ) */
-      masl2xtuml_ooapragma_item_op_populate( ooapopulation->current_pragma, value[0] );
+    /* IF ( not_empty current_element ) */
+    if ( ( 0 != current_element ) ) {
+      /* IF ( ( pragma == current_element.type ) ) */
+      if ( ( Escher_strcmp( "pragma", current_element->type ) == 0 ) ) {
+        /* ooapragma_item::populate( pragma:ooapopulation.current_pragma, value:value[0] ) */
+        masl2xtuml_ooapragma_item_op_populate( ooapopulation->current_pragma, value[0] );
+      }
+    }
+    else {
+      /* TRACE::log( flavor:failure, id:57, message:Element stack is malformed. ) */
+      TRACE_log( "failure", 57, "Element stack is malformed." );
+      /* ooaelement::trace( population:ooapopulation ) */
+      masl2xtuml_ooaelement_op_trace( ooapopulation );
     }
   }
   else {
