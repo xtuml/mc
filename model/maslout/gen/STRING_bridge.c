7c7cb83123318b16bd5d5db474f8ca14a01406ab
Levi Starrett <levi@roxsoftware.com>
2016-04-04 11:41:06 -0400

    job #8404 fixed problems in string functions for parsing pragmas

--- model/maslout/gen/STRING_bridge.c
+++ model/maslout/gen/STRING_bridge.c
@@ -9,47 +9,47 @@
  * your copyright statement can go here (from te_copyright.body)
  *--------------------------------------------------------------------------*/
 
-#include "masl_sys_types.h"
+#include "maslout_sys_types.h"
 #include "LOG_bridge.h"
-#include "out_bridge.h"
 #include "STRING_bridge.h"
 #include "T_bridge.h"
 #include "TRACE_bridge.h"
+#include "V_bridge.h"
 #include "STRING_bridge.h"
-#include "masl_sys_types.h"
+#include "maslout_sys_types.h"
 
 /*
  * Bridge:  itoa
  */
 c_t *
-STRING_itoa( c_t A0xtumlsret[ESCHER_SYS_MAX_STRING_LEN], const i_t p_i )
+STRING_itoa( const i_t p_i )
 {
   /* Replace/Insert the following instructions with your implementation code.  */
-  c_t s[ESCHER_SYS_MAX_STRING_LEN];i_t i;c_t mapping[10][ESCHER_SYS_MAX_STRING_LEN];
+  c_t * s=0;i_t i;c_t * mapping[10]={0};
   /* ASSIGN mapping[9] = 9 */
-  Escher_strcpy( mapping[9], "9" );
+  mapping[9] = Escher_strcpy( mapping[9], "9" );
   /* ASSIGN mapping[8] = 8 */
-  Escher_strcpy( mapping[8], "8" );
+  mapping[8] = Escher_strcpy( mapping[8], "8" );
   /* ASSIGN mapping[7] = 7 */
-  Escher_strcpy( mapping[7], "7" );
+  mapping[7] = Escher_strcpy( mapping[7], "7" );
   /* ASSIGN mapping[6] = 6 */
-  Escher_strcpy( mapping[6], "6" );
+  mapping[6] = Escher_strcpy( mapping[6], "6" );
   /* ASSIGN mapping[5] = 5 */
-  Escher_strcpy( mapping[5], "5" );
+  mapping[5] = Escher_strcpy( mapping[5], "5" );
   /* ASSIGN mapping[4] = 4 */
-  Escher_strcpy( mapping[4], "4" );
+  mapping[4] = Escher_strcpy( mapping[4], "4" );
   /* ASSIGN mapping[3] = 3 */
-  Escher_strcpy( mapping[3], "3" );
+  mapping[3] = Escher_strcpy( mapping[3], "3" );
   /* ASSIGN mapping[2] = 2 */
-  Escher_strcpy( mapping[2], "2" );
+  mapping[2] = Escher_strcpy( mapping[2], "2" );
   /* ASSIGN mapping[1] = 1 */
-  Escher_strcpy( mapping[1], "1" );
+  mapping[1] = Escher_strcpy( mapping[1], "1" );
   /* ASSIGN mapping[0] = 0 */
-  Escher_strcpy( mapping[0], "0" );
+  mapping[0] = Escher_strcpy( mapping[0], "0" );
   /* ASSIGN i = PARAM.i */
   i = p_i;
   /* ASSIGN s =  */
-  Escher_strcpy( s, "" );
+  s = Escher_strcpy( s, "" );
   /* WHILE ( ( i >= 1 ) ) */
   while ( ( i >= 1 ) ) {
     i_t d;
@@ -58,11 +58,11 @@ STRING_itoa( c_t A0xtumlsret[ESCHER_SYS_MAX_STRING_LEN], const i_t p_i )
     /* ASSIGN i = ( i / 10 ) */
     i = ( i / 10 );
     /* ASSIGN s = ( mapping[d] + s ) */
-    Escher_strcpy( s, Escher_stradd( mapping[d], s ) );
+    s = Escher_strcpy( s, Escher_stradd( mapping[d], s ) );
   }
   /* RETURN s */
   {c_t * xtumlOALrv = s;
-  return Escher_strcpy( A0xtumlsret, xtumlOALrv );}
+  return xtumlOALrv;}
 }
 
 
@@ -70,7 +70,7 @@ STRING_itoa( c_t A0xtumlsret[ESCHER_SYS_MAX_STRING_LEN], const i_t p_i )
  * Bridge:  atoi
  */
 i_t
-STRING_atoi( c_t p_s[ESCHER_SYS_MAX_STRING_LEN] )
+STRING_atoi( c_t * p_s )
 {
   /* Replace/Insert the following instructions with your implementation code.  */
   /* RETURN 0 */
@@ -83,11 +83,10 @@ STRING_atoi( c_t p_s[ESCHER_SYS_MAX_STRING_LEN] )
  * Bridge:  substr
  */
 c_t *
-STRING_substr( c_t A0xtumlsret[ESCHER_SYS_MAX_STRING_LEN], const i_t p_begin, const i_t p_end, c_t p_s[ESCHER_SYS_MAX_STRING_LEN] )
+STRING_substr( const i_t p_begin, const i_t p_end, c_t * p_s )
 {
-
   c_t result[ESCHER_SYS_MAX_STRING_LEN];
-  Escher_strcpy( result, "" );
+  result[0] = '\0';
 
   // get length of s
   i_t len = (i_t)Escher_strlen( p_s );
@@ -115,14 +114,15 @@ STRING_substr( c_t A0xtumlsret[ESCHER_SYS_MAX_STRING_LEN], const i_t p_begin, co
 
   }
 
-  return Escher_strcpy( A0xtumlsret, result );
+  return result;
 }
+
 
 /*
  * Bridge:  strlen
  */
 i_t
-STRING_strlen( c_t p_s[ESCHER_SYS_MAX_STRING_LEN] )
+STRING_strlen( c_t * p_s )
 {
   i_t result = 0;
 
@@ -131,11 +131,12 @@ STRING_strlen( c_t p_s[ESCHER_SYS_MAX_STRING_LEN] )
   return result;
 }
 
+
 /*
  * Bridge:  indexof
  */
 i_t
-STRING_indexof( c_t p_haystack[ESCHER_SYS_MAX_STRING_LEN], c_t p_needle[ESCHER_SYS_MAX_STRING_LEN] )
+STRING_indexof( c_t * p_haystack, c_t * p_needle )
 {
   c_t * a;
   c_t * b;
@@ -171,3 +172,69 @@ STRING_indexof( c_t p_haystack[ESCHER_SYS_MAX_STRING_LEN], c_t p_needle[ESCHER_S
   // no match found
   return -1;
 }
+
+
+/*
+ * Bridge:  getword
+ */
+c_t *
+STRING_getword( const i_t p_i, c_t * p_s )
+{
+  c_t result[ESCHER_SYS_MAX_STRING_LEN];
+  result[0] = '\0';
+
+  // check arguments
+  i_t len = (i_t)Escher_strlen( p_s );
+  if ( !(p_i < 0 || p_i > len - 1) ) {
+
+    c_t * w_begin;
+    c_t * w_end;
+    
+    // find the first non comma, whitespace, or close parenthesis
+    w_begin = p_s + p_i;
+    while ( w_begin - p_s < len ) {
+      if ( *w_begin != ',' &&
+           *w_begin != ' ' &&
+           *w_begin != '\n' &&
+           *w_begin != '\r' &&
+           *w_begin != '\t' &&
+           *w_begin != '(' &&
+           *w_begin != ')' ) break;
+      w_begin++;
+    }
+
+    // find the first comma, whitespace, or close parenthesis after starting the word
+    w_end = w_begin;
+    while ( w_end - p_s < len ) {
+      if ( *w_end == ',' ||
+           *w_end == ' ' ||
+           *w_end == '\n' ||
+           *w_end == '\r' ||
+           *w_end == '\t' ||
+           *w_end == '(' ||
+           *w_end == ')' ) break;
+      w_end++;
+    }
+
+    // copy the substring into the result
+    //Escher_strcpy( result, STRING_substr( (const i_t)(w_begin - p_s), (const i_t)(w_end - p_s), p_s ) );
+    c_t * sub = STRING_substr( (const i_t)(w_begin - p_s), (const i_t)(w_end - p_s), p_s );
+
+    // Escher_strcpy
+    c_t * dst = result;
+    c_t * src = sub;
+    if ( 0 != src ) {
+      Escher_size_t i = Escher_strlen( src ) + 1;
+      while ( ( i > 0 ) && ( *src != '\0' ) ) {
+        --i;
+        *dst++ = *src++;
+      }
+      *dst = '\0';  /* Ensure delimiter.  */
+    }
+    //
+    
+  }
+
+  return result;
+}
+
