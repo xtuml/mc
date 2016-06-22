bb1108798c5fdf06846c55e93013496d52ab7972
Levi Starrett <levi@roxsoftware.com>
2016-06-22 16:32:20 -0400

    job #8249 added whole masl directory formatting

--- masl/parser/src/MaslFormatter.java
+++ masl/parser/src/MaslFormatter.java
@@ -8,16 +8,20 @@ public class MaslFormatter {
 
     // public fields
     public static final int FILE = 0;
-    public static final int DOMAIN = 1;
-    public static final int PROJECT = 2;
+    public static final int DIR = 1;
 
     // private fields
     private boolean sort;
     private boolean reorder;
     private int tabwidth;
+    private PrintStream outStream;
 
     // public constructor
     public MaslFormatter() {
+        sort = false;
+        reorder = false;
+        tabwidth = -1;
+        outStream = null;
     }
 
     // setup method
@@ -25,6 +29,84 @@ public class MaslFormatter {
         this.sort = sort;
         this.reorder = reorder;
         this.tabwidth = tabwidth;
+    }
+
+    // parse a directory of MASL files
+    public void parseDir( String indir, String outdir ) {
+
+        // check args
+        if ( null == indir || null == outdir ) {
+            System.err.println( "-parseDir: ERROR null directory" );
+            return;
+        }
+
+        // read the input directory
+        File inputDir = new File( indir );
+        File[] maslFiles = inputDir.listFiles();
+        if ( null == maslFiles ) {
+            System.err.println( "-parseDir: ERROR listFiles() returned null" );
+            return;
+        }
+
+        // get the output directory
+        File outputDir = new File( outdir );
+        if ( outputDir.exists() ) {
+            if ( !outputDir.isDirectory() ) {
+                System.err.println( "-parseDir: ERROR output exists and is not a directory" );
+                return;
+            }
+            if ( !outputDir.canWrite() || !outputDir.canWrite() ) {
+                System.err.println( "-parseDir: ERROR output directory access denied" );
+                return;
+            }
+        }
+        else {
+            try {
+                outputDir.mkdirs();
+            } catch ( SecurityException e ) {
+                System.err.println( "-parseDir: ERROR: " + e );
+                return;
+            }
+        }
+
+        // format all files
+        for ( File f : maslFiles ) {
+            if ( Pattern.matches( ".*\\.svc", f.getName() ) ||
+                 Pattern.matches( ".*\\.fn", f.getName() ) ||
+                 Pattern.matches( ".*\\.ext", f.getName() ) ||
+                 Pattern.matches( ".*\\.scn", f.getName() ) ||
+                 Pattern.matches( ".*\\.al", f.getName() ) ||
+                 Pattern.matches( ".*\\.tr", f.getName() ) ||
+                 Pattern.matches( ".*\\.mod", f.getName() ) ||
+                 Pattern.matches( ".*\\.prj", f.getName() ) ) {
+
+                // get a new file output stream
+                String outpath = outputDir.getPath() + "/" + f.getName();
+                try {
+                    outStream = new PrintStream( new File( outpath ) );
+                } catch ( FileNotFoundException e ) {
+                    System.err.println( "-parseDir: ERROR: " + e );
+                    return;
+                } catch ( SecurityException e ) {
+                    System.err.println( "-parseDir: ERROR: " + e );
+                    return;
+                }
+
+                // get a new input stream
+                InputStream inStream = null;
+                try {
+                    inStream = new FileInputStream( f );
+                } catch ( FileNotFoundException e ) {
+                    System.err.println( "-parseDir: ERROR: " + e );
+                    return;
+                }
+                     
+                // parse the file
+                if ( inStream != null ) parse( inStream );
+            }
+        }
+
+        // done
     }
 
     // parse a MASL file, output formatted MASL
@@ -64,6 +146,7 @@ public class MaslFormatter {
         formatter.setSort( sort );
         formatter.setReorder( reorder );
         if ( tabwidth >= 0 ) formatter.setTabWidth( tabwidth );
+        if ( outStream != null ) formatter.setOut( outStream );
 
         // run formatter
         try {
@@ -80,10 +163,8 @@ public class MaslFormatter {
         System.err.println("Usage:\n");
         System.err.println("Format single MASL file:");
         System.err.println("\tjava -cp <classpath> MaslFormatter [-s] [-r] [-t <tabwidth>] < <input> > <output>\n");
-        System.err.println("Format MASL domain:");
-        System.err.println("\tjava -cp <classpath> MaslImportParser [-s] [-r] [-t <tabwidth>] -d <domain directory> -o <output directory>\n");
-        System.err.println("Format MASL project:");
-        System.err.println("\tjava -cp <classpath> MaslImportParser [-s] [-r] [-t <tabwidth>] -p <project directory> -o <output directory>\n");
+        System.err.println("Format directory:");
+        System.err.println("\tjava -cp <classpath> MaslImportParser [-s] [-r] [-t <tabwidth>] -i <input directory> -o <output directory>\n");
     }
 
     // main method
@@ -97,6 +178,7 @@ public class MaslFormatter {
         int mode = MaslFormatter.FILE;
 
         // get options
+        int i, j;
         if ( Arrays.asList(args).contains("-h") ) {
             formatter.printUsage();
             return;
@@ -105,9 +187,19 @@ public class MaslFormatter {
         if ( Arrays.asList(args).contains("-s") ) sort = true;
         if ( Arrays.asList(args).contains("-r") ) reorder = true;
 
-        int i = Arrays.asList(args).indexOf("-t");
+        i = Arrays.asList(args).indexOf("-t");
         if ( i != -1 && args.length > i+1 ) {
             tabwidth = Integer.parseInt( args[i+1] );
+        }
+
+        i = Arrays.asList(args).indexOf("-i");
+        j = Arrays.asList(args).indexOf("-o");
+        String indir = null;
+        String outdir = null;
+        if ( i != -1 && args.length > i+1 && j != -1 && args.length > j+1 ) {
+            mode = MaslFormatter.DIR;
+            indir = args[i+1];
+            outdir = args[j+1];
         }
         
         // run formatter
@@ -115,9 +207,8 @@ public class MaslFormatter {
         if ( MaslFormatter.FILE == mode ) {
             formatter.parse( System.in );
         }
-        else if ( MaslFormatter.DOMAIN == mode ) {
-        }
-        else if ( MaslFormatter.PROJECT == mode ) {
+        else if ( MaslFormatter.DIR == mode ) {
+            formatter.parseDir( indir, outdir );
         }
         else {
             formatter.printUsage();
