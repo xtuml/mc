ac96bc2d1d205e599bfade3f5609f037a526d693
Levi Starrett <levi@roxsoftware.com>
2016-06-22 14:59:15 -0400

    job #8349 added better commandline usage

--- masl/parser/src/MaslFormatter.java
+++ masl/parser/src/MaslFormatter.java
@@ -2,13 +2,29 @@ import java.io.*;
 import org.antlr.runtime.*;
 import org.antlr.runtime.tree.*;
 import java.util.regex.Pattern;
+import java.util.Arrays;
 
 public class MaslFormatter {
 
+    // public fields
+    public static final int FILE = 0;
+    public static final int DOMAIN = 1;
+    public static final int PROJECT = 2;
+
     // private fields
+    private boolean sort;
+    private boolean reorder;
+    private int tabwidth;
 
     // public constructor
     public MaslFormatter() {
+    }
+
+    // setup method
+    public void setup( boolean sort, boolean reorder, int tabwidth ) {
+        this.sort = sort;
+        this.reorder = reorder;
+        this.tabwidth = tabwidth;
     }
 
     // parse a MASL file, output formatted MASL
@@ -43,9 +59,11 @@ public class MaslFormatter {
 
         nodes = new CommonTreeNodeStream(tree);
         formatter = new MaslFormat( nodes );
+
         formatter.init();
-        formatter.setSort(true);
-        formatter.setReorder(true);
+        formatter.setSort( sort );
+        formatter.setReorder( reorder );
+        if ( tabwidth >= 0 ) formatter.setTabWidth( tabwidth );
 
         // run formatter
         try {
@@ -57,12 +75,53 @@ public class MaslFormatter {
 
     }
 
+    // print usage
+    public void printUsage() {
+        System.err.println("Usage:\n");
+        System.err.println("Format single MASL file:");
+        System.err.println("\tjava -cp <classpath> MaslFormatter [-s] [-r] [-t <tabwidth>] < <input> > <output>\n");
+        System.err.println("Format MASL domain:");
+        System.err.println("\tjava -cp <classpath> MaslImportParser [-s] [-r] [-t <tabwidth>] -d <domain directory> -o <output directory>\n");
+        System.err.println("Format MASL project:");
+        System.err.println("\tjava -cp <classpath> MaslImportParser [-s] [-r] [-t <tabwidth>] -p <project directory> -o <output directory>\n");
+    }
+
     // main method
     public static void main(String args[]) throws Exception {
 
-        MaslFormatter   formatter = new MaslFormatter();    // create new formatter
+        MaslFormatter formatter = new MaslFormatter();      // create new formatter
 
-        formatter.parse( System.in );
+        boolean sort = false;
+        boolean reorder = false;
+        int tabwidth = -1;
+        int mode = MaslFormatter.FILE;
+
+        // get options
+        if ( Arrays.asList(args).contains("-h") ) {
+            formatter.printUsage();
+            return;
+        }
+
+        if ( Arrays.asList(args).contains("-s") ) sort = true;
+        if ( Arrays.asList(args).contains("-r") ) reorder = true;
+
+        int i = Arrays.asList(args).indexOf("-t");
+        if ( i != -1 && args.length > i+1 ) {
+            tabwidth = Integer.parseInt( args[i+1] );
+        }
+        
+        // run formatter
+        formatter.setup( sort, reorder, tabwidth );
+        if ( MaslFormatter.FILE == mode ) {
+            formatter.parse( System.in );
+        }
+        else if ( MaslFormatter.DOMAIN == mode ) {
+        }
+        else if ( MaslFormatter.PROJECT == mode ) {
+        }
+        else {
+            formatter.printUsage();
+        }
 
     }
 }
