ab51bd1ff74116dcfa3e2f61d32ebd34c14ed142
Cortland Starrett <cort@roxsoftware.com>
2017-01-24 17:46:22 -0500

    job:  #8980  This commit is successfully creating and relating instances of Markable, Feature and Mark.  The path and value attributes are not correct.  Default (empty) mc3020 marking files are removed.  The change to the MC to avoid ooapopulation instance loading and dumping is reset, because we can simply make sure that there are no ooapopulation instances before we exit.

--- arc/t.class.instancedumper.c
+++ arc/t.class.instancedumper.c
@@ -5,10 +5,7 @@
 void
 ${te_class.GeneratedName}_instancedumper( ${te_instance.handle} instance )
 {
-.// hack to help the build of maslin
-.if ( "ooapopulation" != te_class.Key_Lett )
   ${te_class.GeneratedName} * self = (${te_class.GeneratedName} *) instance;
   printf( "INSERT INTO ${te_class.Key_Lett} VALUES ( ${te_class.attribute_format} );\n"${te_class.attribute_dump} );
-.end if
 }
 .end if
