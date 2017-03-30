86ce81fb06a59df281228d2edcfdd42c05a1814d
Levi Starrett <levi@roxsoftware.com>
2017-03-30 15:54:11 -0400

    job #9398 Updated CLI and MASL test scripts to integrate new 'Launch' CLI feature

--- bin/CLI.sh
+++ bin/CLI.sh
@@ -13,7 +13,7 @@ function die() {
 
 function displayHelp() {
   echo -e ""
-  echo -e "Usage:  ./CLI.sh <Build | Import | Execute> <Argument List>"
+  echo -e "Usage:  ./CLI.sh <Build | Import | Execute | Merge | Launch> <Argument List>"
   echo -e ""
   echo -e "Example:"
   echo -e "  ./CLI.sh Build -project MicrowaveOven -buildConfig Debug -cleanCLI"
@@ -23,6 +23,7 @@ function displayHelp() {
   echo -e "   ./CLI.sh Import -help"
   echo -e "   ./CLI.sh Execute -help"
   echo -e "   ./CLI.sh Merge -help"
+  echo -e "   ./CLI.sh Launch -help"
   echo -e ""
   echo -e "Note that you must edit the WORKSPACE variable inside this CLI.sh"
   echo -e "to point at the appropriate workspace."
@@ -38,7 +39,9 @@ if [ "$1" != "Build" ]; then
   if [ "$1" != "Execute" ]; then
     if [ "$1" != "Import" ]; then
        if [ "$1" != "Merge" ]; then
-          displayHelp
+           if [ "$1" != "Launch" ]; then
+              displayHelp
+           fi
        fi
     fi
   fi
@@ -59,8 +62,57 @@ fi
 # DO NOT MODIFY ANY OF THE FOLLOWING LINES.
 #
 APPLICATION="org.xtuml.bp.cli.$1"
-BP_JVM=$BPHOMEDIR/jre/bin/java
+export JVM_ARG="-vm $BPHOMEDIR/jre/bin/java"
+if [[ `uname` == "Darwin" ]]; then
+    export JVM_ARG=""
+fi
 
-$BPHOMEDIR/bridgepoint --launcher.suppressErrors -vm $BP_JVM -clean -noSplash -data $WORKSPACE -application $APPLICATION $2 "$3" $4 "$5" $6 "$7" $8 "$9"
+# handle Launch commands
+export BPHOME=$BPHOMEDIR
+CLI_FILE=$DIR/CLI_PORT.txt
+if [ "$1" == "Launch" ]; then
+    # exit the launched eclipse instance
+    if [[ $# -eq 2 && "$2" == "-exit" ]]; then
+        stat $CLI_FILE &> /dev/null
+        if [ $? -eq 0 ]; then
+            python $DIR/launch-cli.py cmd `cat $CLI_FILE` exit
+            rm -f $CLI_FILE
+        else
+            echo "No command line instance running"
+            exit 1
+        fi
+    # launch an instance
+    elif [ $# -eq 1 ]; then
+        python $DIR/launch-cli.py launch $CLI_FILE
+        chmod 400 $CLI_FILE
+    # show launch command help
+    elif [[ $# -eq 2 && "$2" == "-help" ]]; then
+        echo "USAGE:"
+        echo
+        echo "./CLI.sh Launch"
+        echo
+        echo "    Launch a command line instance of BridgePoint in the background. Following"
+        echo "    calls to CLI.sh will attach to this instance."
+        echo
+        echo "./CLI.sh Launch -exit"
+        echo
+        echo "    Terminate the current running CLI instance"
+        echo
+        echo "./CLI.sh Launch -help"
+        echo
+        echo "    Display command help"
+    else
+        displayHelp
+    fi
+else
+    stat $CLI_FILE &> /dev/null
+    if [ $? -eq 0 ]; then
+        # an instance is running, attach to it
+        python $DIR/launch-cli.py cmd `cat $CLI_FILE` ${@:1}
+    else
+        # run the normal way
+        $BPHOMEDIR/bridgepoint --launcher.suppressErrors $JVM_ARG -clean -noSplash -data $WORKSPACE -application $APPLICATION $2 "$3" $4 "$5" $6 "$7" $8 "$9"
+    fi
+fi
  
 exit 0
