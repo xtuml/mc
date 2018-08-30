34b478681960ff017d79b2180e44e64a4c9e4692
Cortland Starrett <cort@roxsoftware.com>
2018-08-30 14:39:52 -0400

    job:  #10366  #10351  Re-remove unused files.

--- model/escher/gen/c/t.smt_sr.declare_ref.c
+++
@@ -1,8 +0,0 @@
-T_b(te_class->GeneratedName);
-T_b(" * ");
-T_b(te_select_related->result_var);
-T_b(" = 0; /* ");
-T_b(te_select_related->result_var_OAL);
-T_b(" (");
-T_b(te_class->Key_Lett);
-T_b(") */\n");
