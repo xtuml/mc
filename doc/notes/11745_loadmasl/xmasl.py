78acbea1752447c624ef7b696079881b38998342
Cortland Starrett <cort@roxsoftware.com>
2019-12-13 15:25:25 -0500

    job:  #11745  This updated script creates .mod and .int files for all masl metamodelImpl subsystems.  It includes the classes, attributes, imported classes and relationships as directed directly from the java.  Attributes with names 'type' and 'anonymous' are coerced.  Lists and sub/super still remain to be enhanced.  Even at this point, this saves a lot of editing.

--- doc/notes/11745_loadmasl/xmasl.py
+++ doc/notes/11745_loadmasl/xmasl.py
@@ -1,50 +1,111 @@
-import sys
-import os
-import subprocess
+import sys, os, subprocess, re
 
-def usage(incorrect=""):
-  if "" != incorrect:
-    print "Incorrect usage '" + incorrect + "'"
-  print "Usage:"
-  print "    python " + sys.argv[0] + " objects <filename>"
-  sys.exit(1)
+ofilepath = sys.argv[1]
+if ( not os.path.isdir( ofilepath ) ):
+  os.mkdir( ofilepath )
+root = '/Users/cort/git/masl/core-java/src/main/java/org/xtuml/masl/metamodelImpl'
+dirs = [ d for d in os.listdir( root ) if not os.path.isfile( os.path.join( root, d ) ) ]
 
-if len(sys.argv) > 2:
-  if "objects" == sys.argv[1].lower():
-    object_list = sys.argv[2]
-    print "listing " + object_list
-    # Open the input file of object names.
-    f = open(object_list, "r")
-    lines = f.readlines()
-    f.close()
-    rootDir = '/Users/cort/git/masl/core-java/src/main/java/org/xtuml/masl/metamodelImpl'
-    for line in lines:
-      objectname = line.rstrip()
-      print objectname
-      pattern = ' p.* ' + objectname + ' \\( '
-      r = ""
-      for dirName, subdirList, fileList in os.walk( rootDir ):
-        cmd = ""
-        for fname in fileList:
-          cmd = "awk '/" + pattern + "/,/\)/{print $0;}' " + dirName + '/' + fname
-          if ( fname == objectname + '.java' ):
-            #print cmd
-            try:
-              r = subprocess.check_output(cmd, shell=True)
-            except Exception, e:
-              r = str(e.output)
-      r = r.rstrip()
-      r = r.replace( "private", "" )
-      r = r.replace( "public", "" )
-      r = r.replace( "final", "" )
-      print r
+# Build list of objects.
+objectlist = []
+# subsystems
+subsystem = [list() for _ in xrange(20)]
 
-  elif "another..." == sys.argv[1].lower():
-    if len(sys.argv) > 3:
-      usage(" ".join(sys.argv))
-    else:
-      usage(" ".join(sys.argv))
-  else:
-    usage()
-else:
-    usage()
+# Initialize objectlist and subsystem lists.
+subsystemindex = 0
+for d in sorted( dirs ):
+  dpath = os.path.join( root, d )
+  files = [ f for f in os.listdir( dpath ) if os.path.isfile( os.path.join( dpath, f ) ) ]
+  for fname in sorted( files ):
+    objectname = os.path.splitext( fname )[0]
+    objectlist.append( objectname )
+    subsystem[ subsystemindex ].append( objectname )
+  subsystemindex = subsystemindex + 1
+
+relnum = 1
+subsystemindex = 0
+for d in sorted( dirs ):
+  dpath = os.path.join( root, d )
+  objectdeclaration = ""
+  typelist = ""
+  relspecs = ""
+  objectdefinition = ""
+  importedobjects = set()
+  importedobjectlist = ""
+  files = [ f for f in os.listdir( dpath ) if os.path.isfile( os.path.join( dpath, f ) ) ]
+  for fname in sorted( files ):
+    objectname = os.path.splitext( fname )[0]
+    objectdeclaration = objectdeclaration + '  object ' + objectname + ';\n'
+    typelist = typelist + '  private type i' + objectname + ' is instance of ' + objectname + ';\n'
+    pattern = ' p.* ' + objectname + ' \\( '
+    cmd = "awk '/" + pattern + "/,/\)/{print $0;}' " + dpath + '/' + fname
+    attrs = ""
+    try:
+      attrs = subprocess.check_output(cmd, shell=True)
+    except Exception, e:
+      attrs = str( e.output )
+    attrs = attrs.rstrip()
+    attrs = attrs.replace( "private", "" )
+    attrs = attrs.replace( "protected", "" )
+    attrs = attrs.replace( "public", "" )
+    attrs = attrs.replace( "final", "" )
+    attrs = attrs.replace( "static", "" )
+    attrs = attrs.replace( "throws SemanticError", "" )
+    attrs = re.sub( r'.*\(', '', attrs )
+    attrs = attrs.replace( ")", "" )
+    attrs = attrs.replace( ", ", "\n " )
+    attrs = attrs.replace( ",", "" )
+    attrs = re.sub( ' +', ' ', attrs )
+    attrs = re.sub( r'^\n', '', attrs )
+    alines = attrs.splitlines()
+    alines = [line for line in alines if line.strip()]
+    # Remove duplicates.
+    alines = set(alines)
+    attrs = ""
+    for line in alines:
+      attrcomment = ""
+      words = line.split(" ")
+      emptyness = words[0]
+      atype = words[1]
+      aname = words[2]
+      if ( atype in objectlist ):
+        if ( atype not in subsystem[ subsystemindex ] ):
+          importedobjects.add( atype )
+        if ( atype != 'Position' ):
+          attrcomment = "    //!" + objectname + " is related to " + atype + "\n"
+          relspec = "relationship R" + str(relnum) + " is " + objectname + " unconditionally XX one " + atype + ", " + atype + " unconditionally YY one " + objectname + ";\n"
+          relspecs = relspecs + "  " + relspec
+          attrcomment = "    //!" + relspec
+          relnum = relnum + 1
+        atype = "i" + atype
+      if ( aname == 'anonymous' ):
+        attrcomment = "    //!" + aname + "\n"
+        aname = "is_" + aname
+      elif ( aname == 'type' ):
+        attrcomment = "    //!" + aname + "\n"
+        aname = "R_" + aname
+      attrs = attrs + attrcomment
+      attrs = attrs + "    " + aname + " : " + atype + ";\n"
+    objectdefinition = objectdefinition + '  object ' + objectname + ' is\n' + attrs + '  end object;\n'
+
+  for iobj in importedobjects:
+    objectdeclaration = objectdeclaration + '  object Imported_' + iobj + ';\n'
+    importedobjectlist = importedobjectlist + '  object Imported_' + iobj + ' is\n  end object;\n'
+
+  # Write the subsystem file as a domain.
+  dfilepath = os.path.join( ofilepath, d + "s" )
+  if ( not os.path.isdir( dfilepath ) ):
+    os.mkdir( dfilepath )
+  dfilename = os.path.join( dfilepath, d + "s" )
+  with open( dfilename + '.int', 'w' ) as intfile:
+    intfile.write( 'domain ' + d + 's is\nend domain;\n' )
+  with open( dfilename + '.mod', 'w' ) as modfile:
+    modfile.write( 'domain ' + d + 's is\n' )
+    modfile.write( objectdeclaration )
+    modfile.write( typelist )
+    modfile.write( relspecs )
+    modfile.write( objectdefinition )
+    modfile.write( '  //! imported objects\n' )
+    modfile.write( importedobjectlist )
+    modfile.write( 'end domain;\n' )
+  subsystemindex = subsystemindex + 1
