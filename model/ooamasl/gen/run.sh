9d2081deb14b96fb6743b151afe86366be00e8b5
Cortland Starrett <cort@roxsoftware.com>
2020-01-24 14:42:14 -0500

    job:  #11745  beginning to experiment building the model with ciera, added an init function

---
+++ model/ooamasl/gen/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+CIERA_VERSION=1.2.0
+CLASSPATH=~/.m2/repository/io/ciera/runtime/$CIERA_VERSION/runtime-$CIERA_VERSION.jar:~/.m2/repository/io/ciera/ooamasl/1.0.0-SNAPSHOT/ooamasl-1.0.0-SNAPSHOT.jar
+java -cp $CLASSPATH ooamasl.OoamaslApplication $@
