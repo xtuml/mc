47d60702394f4fa57b836a2ac75339936eaa4dfd
Cortland Starrett <cort@roxsoftware.com>
2020-01-27 13:01:54 -0500

    job:  #11745  Removed deploy package from ooamasl.  Now this lives in the stratus.  ooamasl is a "naked" OOA (no action language or deployment configurations).

--- model/ooamasl/gen/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-CIERA_VERSION=1.2.0-SNAPSHOT
-CLASSPATH=~/.m2/repository/io/ciera/runtime/$CIERA_VERSION/runtime-$CIERA_VERSION.jar:~/.m2/repository/io/ciera/ooamasl/1.0.0-SNAPSHOT/ooamasl-1.0.0-SNAPSHOT.jar
-java -cp $CLASSPATH ooamasl.OoamaslApplication $@
