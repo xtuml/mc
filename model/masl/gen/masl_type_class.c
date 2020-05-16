091699bfe2ea801fed63017ca8dd3efe42a8a99a
Cortland Starrett <cort@roxsoftware.com>
2020-05-16 16:56:32 -0400

    job:  #11931  Properly output the lines of the .brp file (added newline).

--- model/masl/gen/masl_type_class.c
+++ model/masl/gen/masl_type_class.c
@@ -69,30 +69,35 @@ masl_type_AnyWhere1( c_t * w_name )
 void
 masl_type_op_render( masl_type * self)
 {
-  masl_reference * reference=0;masl_description * descrip=0;masl_structure * structure=0;masl_enumeration * enumeration=0;masl_markable * markable=0;Escher_ObjectSet_s references_space={0}; Escher_ObjectSet_s * references = &references_space;Escher_ObjectSet_s descrips_space={0}; Escher_ObjectSet_s * descrips = &descrips_space;
-  /* SELECT many descrips RELATED BY self->markable[R3783]->element[R3786]->description[R3796] */
-  Escher_ClearSet( descrips );
-  {  if ( 0 != self ) {
-  masl_markable * markable_R3783 = self->markable_R3783;
-  if ( 0 != markable_R3783 ) {
-  masl_element * element_R3786 = markable_R3783->element_R3786;
-  if ( 0 != element_R3786 ) {
-  masl_description * description_R3796;
-  Escher_Iterator_s idescription_R3796;
-  Escher_IteratorReset( &idescription_R3796, &element_R3786->description_R3796 );
-  while ( 0 != ( description_R3796 = (masl_description *) Escher_IteratorNext( &idescription_R3796 ) ) ) {
-    if ( ! Escher_SetContains( (Escher_ObjectSet_s *) descrips, description_R3796 ) ) {
-      Escher_SetInsertElement( (Escher_ObjectSet_s *) descrips, description_R3796 );
-  }}}}}}
-  /* FOR EACH descrip IN descrips */
-  { Escher_Iterator_s iterdescrip;
-  masl_description * iidescrip;
-  Escher_IteratorReset( &iterdescrip, descrips );
-  while ( (iidescrip = (masl_description *)Escher_IteratorNext( &iterdescrip )) != 0 ) {
-    descrip = iidescrip; {
-    /* descrip.render() */
-    masl_description_op_render( descrip );
-  }}}
+  masl_reference * reference=0;masl_structure * structure=0;masl_enumeration * enumeration=0;masl_markable * markable=0;Escher_ObjectSet_s references_space={0}; Escher_ObjectSet_s * references = &references_space;
+  /* IF ( MASL == genfile::architecture() ) */
+  if ( Escher_strcmp( "MASL", masl_genfile_op_architecture() ) == 0 ) {
+    masl_description * descrip=0;Escher_ObjectSet_s descrips_space={0}; Escher_ObjectSet_s * descrips = &descrips_space;
+    /* SELECT many descrips RELATED BY self->markable[R3783]->element[R3786]->description[R3796] */
+    Escher_ClearSet( descrips );
+    {    if ( 0 != self ) {
+    masl_markable * markable_R3783 = self->markable_R3783;
+    if ( 0 != markable_R3783 ) {
+    masl_element * element_R3786 = markable_R3783->element_R3786;
+    if ( 0 != element_R3786 ) {
+    masl_description * description_R3796;
+    Escher_Iterator_s idescription_R3796;
+    Escher_IteratorReset( &idescription_R3796, &element_R3786->description_R3796 );
+    while ( 0 != ( description_R3796 = (masl_description *) Escher_IteratorNext( &idescription_R3796 ) ) ) {
+      if ( ! Escher_SetContains( (Escher_ObjectSet_s *) descrips, description_R3796 ) ) {
+        Escher_SetInsertElement( (Escher_ObjectSet_s *) descrips, description_R3796 );
+    }}}}}}
+    /* FOR EACH descrip IN descrips */
+    { Escher_Iterator_s iterdescrip;
+    masl_description * iidescrip;
+    Escher_IteratorReset( &iterdescrip, descrips );
+    while ( (iidescrip = (masl_description *)Escher_IteratorNext( &iterdescrip )) != 0 ) {
+      descrip = iidescrip; {
+      /* descrip.render() */
+      masl_description_op_render( descrip );
+    }}}
+    Escher_ClearSet( descrips ); 
+  }
   /* ASSIGN self.rendered = TRUE */
   ((masl_type *)xtUML_detect_empty_handle( self, "type", "self.rendered" ))->rendered = TRUE;
   /* SELECT many references RELATED BY self->reference[R3777.is_referenced_by] */
@@ -145,7 +150,7 @@ masl_type_op_render( masl_type * self)
   }
   /* markable.render_marking( list:default ) */
   masl_markable_op_render_marking( markable,  "default" );
-  Escher_ClearSet( references ); Escher_ClearSet( descrips ); 
+  Escher_ClearSet( references ); 
 }
 
 /*
