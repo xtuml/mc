802a8ec31cb37a38f976917674ed5604599a1848
Levi Starrett <levi@roxsoftware.com>
2020-11-12 23:57:23 -0500

    add long_description

--- pymc/setup.py
+++ pymc/setup.py
@@ -5,9 +5,16 @@ try:
 except ImportError:
     from distutils.core import setup
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='pymc3020',
       version='1.0.1',
       description='Python executor for MC-3020',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Levi Starrett',
       author_email='levi@roxsoftware.com',
       url='https://github.com/xtuml/mc',
