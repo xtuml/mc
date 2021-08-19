97b62d98e6f4980ec7b45f18d5b6b365a93f2ed7
Levi Starrett <levi@roxsoftware.com>
2021-08-19 10:49:43 -0400

    job #12234 move ooaofmasl arch packages into one package for easier import

--- model/stratus/gen/run.sh
+++ model/stratus/gen/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-CIERA_VERSION=2.0.1-SNAPSHOT
+CIERA_VERSION=2.1.0
 CLASSPATH=~/.m2/repository/org/antlr/antlr-runtime/3.5.2/antlr-runtime-3.5.2.jar:~/.m2/repository/io/ciera/runtime/$CIERA_VERSION/runtime-$CIERA_VERSION.jar:~/.m2/repository/org/xtuml/stratus/1.0.0-SNAPSHOT/stratus-1.0.0-SNAPSHOT.jar
 java -ea -cp $CLASSPATH stratus.StratusApplication $@
