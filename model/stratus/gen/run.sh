9dbf219dca00da0d0cd149a3d7936b12b06c1833
Levi Starrett <levi@roxsoftware.com>
2021-08-20 11:12:34 -0400

    bump versions

--- model/stratus/gen/run.sh
+++ model/stratus/gen/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 CIERA_VERSION=2.4.0
-CLASSPATH=~/.m2/repository/org/antlr/antlr-runtime/3.5.2/antlr-runtime-3.5.2.jar:~/.m2/repository/io/ciera/runtime/$CIERA_VERSION/runtime-$CIERA_VERSION.jar:~/.m2/repository/org/xtuml/stratus/1.0.0-SNAPSHOT/stratus-1.0.0-SNAPSHOT.jar
+CLASSPATH=~/.m2/repository/org/antlr/antlr-runtime/3.5.2/antlr-runtime-3.5.2.jar:~/.m2/repository/io/ciera/runtime/$CIERA_VERSION/runtime-$CIERA_VERSION.jar:~/.m2/repository/org/xtuml/stratus/1.0.1-SNAPSHOT/stratus-1.0.1-SNAPSHOT.jar
 java -ea -cp $CLASSPATH stratus.StratusApplication $@
