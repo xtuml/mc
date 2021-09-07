3a9866a6f788c14fb16cac80e7f5b5d5e14ee028
Levi Starrett <levi@roxsoftware.com>
2021-09-07 10:20:39 -0400

    TEMP setup

--- model/stratus/gen/run.sh
+++ model/stratus/gen/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 CIERA_VERSION=2.4.0
-CLASSPATH=~/.m2/repository/org/antlr/antlr-runtime/3.5.2/antlr-runtime-3.5.2.jar:~/.m2/repository/io/ciera/runtime/$CIERA_VERSION/runtime-$CIERA_VERSION.jar:~/.m2/repository/org/xtuml/stratus/1.0.0-SNAPSHOT/stratus-1.0.0-SNAPSHOT.jar
+ANTLR_VERSION=3.5.2
+STRATUS_VERSION=1.0.0
+CLASSPATH=~/.m2/repository/org/antlr/antlr-runtime/$ANTLR_VERSION/antlr-runtime-$ANTLR_VERSION.jar:~/.m2/repository/io/ciera/runtime/$CIERA_VERSION/runtime-$CIERA_VERSION.jar:~/.m2/repository/org/xtuml/stratus/$STRATUS_VERSION/stratus-$STRATUS_VERSION.jar
 java -ea -cp $CLASSPATH stratus.StratusApplication $@
