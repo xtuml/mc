12199c1ef7eb02b6011803b2feeb6403b96c2cb4
Levi Starrett <levi@roxsoftware.com>
2021-10-08 00:23:44 -0400

    update project setup

--- model/stratus/gen/run.sh
+++ model/stratus/gen/run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-CIERA_VERSION=2.4.0
+MAVEN_HOME=~/.m2/repository
+CIERA_VERSION=2.6.2-SNAPSHOT
 ANTLR_VERSION=4.9.1
-STRATUS_VERSION=1.0.0-SNAPSHOT
-CLASSPATH=~/.m2/repository/org/antlr/antlr4-runtime/$ANTLR_VERSION/antlr4-runtime-$ANTLR_VERSION.jar:~/.m2/repository/io/ciera/runtime/$CIERA_VERSION/runtime-$CIERA_VERSION.jar:~/.m2/repository/org/xtuml/stratus/$STRATUS_VERSION/stratus-$STRATUS_VERSION.jar
+STRATUS_VERSION=1.0.1-SNAPSHOT
+CLASSPATH=$MAVEN_HOME/org/antlr/antlr4-runtime/$ANTLR_VERSION/antlr4-runtime-$ANTLR_VERSION.jar:$MAVEN_HOME/io/ciera/runtime/$CIERA_VERSION/runtime-$CIERA_VERSION.jar:$MAVEN_HOME/org/xtuml/stratus/$STRATUS_VERSION/stratus-$STRATUS_VERSION.jar
 java -ea -cp $CLASSPATH stratus.StratusApplication $@
 #java -ea -cp $CLASSPATH deploy.parser.MaslImportParser $@
